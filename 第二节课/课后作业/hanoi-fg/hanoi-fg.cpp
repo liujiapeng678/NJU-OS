802275c088b9ad983addb9f6c7081e75c5f89984
MOSS <jj0723938@gmail.com>
2024-08-04 11:59:28 +0800

    Update hanoi-fg.cpp

--- 第二节课/课后作业/hanoi-fg/hanoi-fg.cpp
+++ 第二节课/课后作业/hanoi-fg/hanoi-fg.cpp
@@ -1,1 +1,14 @@
 
+#include <cstdio>
+#include "hanoi-fgFun.h"
+
+int main() {
+    int n = 5;
+
+    fg_no_re(n, 'f');
+    printf("f_no_re = %d, ", sum);
+    fg_no_re(n, 'g');
+    printf("g_no_re = %d\n", sum);
+
+    return 0;
+}
